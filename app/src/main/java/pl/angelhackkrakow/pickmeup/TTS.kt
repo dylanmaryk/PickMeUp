9611a26652e07ab552b3ddc582e83e07598ea789
Jacek Kuzniarski <jacek.kuzniarski@infullmobile.com>
2018-05-19 17:05:19 +0200

    Mood listener

--- app/src/main/java/pl/angelhackkrakow/pickmeup/TTS.kt
+++ app/src/main/java/pl/angelhackkrakow/pickmeup/TTS.kt
@@ -1,19 +1,18 @@
 package pl.angelhackkrakow.pickmeup
 
 import android.content.Context
+import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
+import java.util.*
 
 class TTS(
         private val context: Context
 ) {
-
-
     private var textToSpeech: TextToSpeech = TextToSpeech(context, TextToSpeech.OnInitListener { })
 
-
     @JvmOverloads fun speak(text: String, onDone: Runnable? = null) {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, Bundle.EMPTY, UUID.randomUUID().toString())
         textToSpeech.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
             override fun onStart(s: String) {
 
