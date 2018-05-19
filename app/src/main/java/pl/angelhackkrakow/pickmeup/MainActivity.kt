cbd76d2239590ddad387039703b05f42442af642
Dylan Maryk <dylan@door2door.io>
2018-05-19 17:58:29 +0200

    Undo merged changes

--- app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
+++ app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
@@ -1,91 +1,59 @@
 package pl.angelhackkrakow.pickmeup
 
+import ai.api.AIListener
 import ai.api.android.AIConfiguration
+import ai.api.android.AIService
 import ai.api.model.AIError
 import ai.api.model.AIResponse
-import ai.api.ui.AIButton
+import android.Manifest
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val tts by lazy { TTS(this) }
-    private val config by lazy {
-        AIConfiguration(DIALOG_FLOW_TOKEN,
-                ai.api.AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initMicButton()
-        tts.speak("Hi Mike, how are you feeling today?")
-    }
-
-    private fun initMicButton() {
-        micButton.initialize(config)
-        micButton.setResultsListener(createMoodListener())
-    }
-
-    private fun createMoodListener(): MoodListener {
-        return MoodListener { speech -> tts.speak(speech) }
-                .apply {
-                    onGoodMood = { proceedGoodMood() }
-                    onOkMood = { proceedOkMood() }
-                    onBadMood = { proceedBadMood() }
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                1)
+        viewAnimator.setInAnimation(this, android.R.anim.fade_in)
+        viewAnimator.setOutAnimation(this, android.R.anim.fade_out)
+        view.setOnClickListener {
+            viewAnimator.showNext()
+            val config = AIConfiguration(DIALOG_FLOW_TOKEN,
+                    ai.api.AIConfiguration.SupportedLanguages.English,
+                    AIConfiguration.RecognitionEngine.System)
+            val aiService = AIService.getService(this, config)
+            aiService.setListener(object : AIListener {
+                override fun onResult(result: AIResponse?) {
+                    val speech = result?.result?.fulfillment?.speech
+                    speech?.let {
+                        tts.speak(speech)
+                    }
+                    viewAnimator.showNext()
                 }
-    }
 
-    private fun proceedBadMood() {
+                override fun onListeningStarted() {
+                }
 
-    }
+                override fun onAudioLevel(level: Float) {
+                }
 
-    private fun proceedOkMood() {
+                override fun onError(error: AIError?) {
+                }
 
-    }
+                override fun onListeningCanceled() {
+                }
 
-    private fun proceedGoodMood() {
-
-    }
-
-    class MoodListener(val speech: (String) -> Unit) : AIButton.AIButtonListener {
-
-        var onGoodMood: () -> Unit = {}
-        var onOkMood: () -> Unit = {}
-        var onBadMood: () -> Unit = {}
-
-
-        override fun onCancelled() {
-        }
-
-        override fun onResult(result: AIResponse) {
-            speech(result.result.fulfillment.speech)
-            processMood(Mood.from(result.result.action))
-        }
-
-        private fun processMood(mood: Mood) {
-            when (mood) {
-                Mood.BAD -> onBadMood()
-                Mood.OK -> onOkMood()
-                Mood.GOOD -> onGoodMood()
-            }
-        }
-
-        override fun onError(error: AIError?) {
-        }
-    }
-
-    enum class Mood(val mood: String) {
-        BAD("BadMood"), OK("OkMood"), GOOD("GoodMood");
-
-        companion object {
-
-            fun from(value: String): Mood {
-                return Mood.values()
-                        .firstOrNull { mood -> mood.mood.equals(value, true) } ?: OK
-            }
+                override fun onListeningFinished() {
+                }
+            })
+            aiService.startListening()
         }
     }
 
