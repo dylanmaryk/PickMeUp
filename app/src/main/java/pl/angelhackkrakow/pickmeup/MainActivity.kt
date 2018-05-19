d1e12b6ce46564db7568ff0245e55af676f8a75a
Jacek Kuzniarski <jacek.kuzniarski@infullmobile.com>
2018-05-19 17:05:19 +0200

    Mood listener

--- app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
+++ app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
@@ -4,41 +4,88 @@ import ai.api.android.AIConfiguration
 import ai.api.model.AIError
 import ai.api.model.AIResponse
 import ai.api.ui.AIButton
-import android.Manifest
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val tts by lazy { TTS(this) }
+    private val config by lazy {
+        AIConfiguration(DIALOG_FLOW_TOKEN,
+                ai.api.AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                1)
-        val config = AIConfiguration(DIALOG_FLOW_TOKEN,
-                ai.api.AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System)
+        initMicButton()
+        tts.speak("Hi Mike, how are you feeling today?")
+    }
+
+    private fun initMicButton() {
         micButton.initialize(config)
-        micButton.setResultsListener(object : AIButton.AIButtonListener {
-            override fun onCancelled() {
-            }
+        micButton.setResultsListener(createMoodListener())
+    }
 
-            override fun onResult(result: AIResponse) {
-                textView.text = Gson().toJson(result)
-                val speech = result.result.fulfillment.speech
-                tts.speak(speech)
-            }
+    private fun createMoodListener(): MoodListener {
+        return MoodListener { speech -> tts.speak(speech) }
+                .apply {
+                    onGoodMood = { proceedGoodMood() }
+                    onOkMood = { proceedOkMood() }
+                    onBadMood = { proceedBadMood() }
+                }
+    }
 
-            override fun onError(error: AIError?) {
+    private fun proceedBadMood() {
+
+    }
+
+    private fun proceedOkMood() {
+
+    }
+
+    private fun proceedGoodMood() {
+    }
+
+    class MoodListener(val speech: (String) -> Unit) : AIButton.AIButtonListener {
+
+        var onGoodMood: () -> Unit = {}
+        var onOkMood: () -> Unit = {}
+        var onBadMood: () -> Unit = {}
+
+
+        override fun onCancelled() {
+        }
+
+        override fun onResult(result: AIResponse) {
+            speech(result.result.fulfillment.speech)
+            processMood(Mood.from(result.result.action))
+        }
+
+        private fun processMood(mood: Mood) {
+            when (mood) {
+                Mood.BAD -> onBadMood()
+                Mood.OK -> onOkMood()
+                Mood.GOOD -> onGoodMood()
             }
-        })
+        }
+
+        override fun onError(error: AIError?) {
+        }
+    }
+
+    enum class Mood(val mood: String) {
+        BAD("BadMood"), OK("OkMood"), GOOD("GoodMood");
+
+        companion object {
+
+            fun from(value: String): Mood {
+                return Mood.values()
+                        .firstOrNull { mood -> mood.mood.equals(value, true) } ?: OK
+            }
+        }
     }
 
     companion object {
