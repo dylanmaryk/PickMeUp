abd81cabd6c185e511b81f1f9e40d5fc07863227
Jacek Kuzniarski <jacek.kuzniarski@infullmobile.com>
2018-05-19 19:36:47 +0200

    Almost full

--- app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
+++ app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
@@ -5,63 +5,135 @@ import ai.api.android.AIConfiguration
 import ai.api.android.AIService
 import ai.api.model.AIError
 import ai.api.model.AIResponse
-import android.Manifest
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val aiService by lazy {
+        AIService.getService(this, config)
+    }
+
+    private val config by lazy {
+        AIConfiguration(DIALOG_FLOW_TOKEN,
+                ai.api.AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System)
+    }
     private val tts by lazy { TTS(this) }
+
+    private var displayedView: Int
+        get() = viewAnimator.displayedChild
+        set(value) {
+            viewAnimator.displayedChild = value
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                1)
-        viewAnimator.setInAnimation(this, android.R.anim.fade_in)
-        viewAnimator.setOutAnimation(this, android.R.anim.fade_out)
-        view.setOnClickListener {
-            viewAnimator.showNext()
-            val config = AIConfiguration(DIALOG_FLOW_TOKEN,
-                    ai.api.AIConfiguration.SupportedLanguages.English,
-                    AIConfiguration.RecognitionEngine.System)
-            val aiService = AIService.getService(this, config)
-            aiService.setListener(object : AIListener {
-                override fun onResult(result: AIResponse?) {
-                    val speech = result?.result?.fulfillment?.speech
-                    speech?.let {
-                        tts.speak(it)
-                    }
-                    val resolvedQuery = result?.result?.resolvedQuery
-                    resolvedQuery?.let {
-                        feeling_goo.text = it
-                    }
-                    viewAnimator.showNext()
-                }
+        tts.init(INIT_SPEECH, {
+            runOnUiThread {
+                displayedView = LISTENING
+                startListening()
+            }
+        })
+    }
 
-                override fun onListeningStarted() {
-                }
+    private fun startListening() {
+        aiService.setListener(createMoodListener())
+        aiService.startListening()
+    }
 
-                override fun onAudioLevel(level: Float) {
+    private fun createMoodListener(): MoodListener {
+        return MoodListener(
+                { wasSaid -> showWhatSaid(wasSaid) },
+                { sayThis -> tts.speak(sayThis) })
+                .apply {
+                    onGoodMood = { proceedGoodMood() }
+                    onOkMood = { proceedOkMood() }
+                    onBadMood = { proceedBadMood() }
                 }
+    }
 
-                override fun onError(error: AIError?) {
-                }
+    private fun showWhatSaid(speech: String) {
+        displayedView = WHAT_WAS_SAID
+        whatWeSaid.text = speech
+    }
 
-                override fun onListeningCanceled() {
-                }
+    private fun proceedBadMood() {
+        showWhatSaid("Bad mood")
+    }
 
-                override fun onListeningFinished() {
-                }
-            })
-            aiService.startListening()
+    private fun proceedOkMood() {
+        showWhatSaid("Ok mood")
+    }
+
+    private fun proceedGoodMood() {
+        showWhatSaid("Good mood")
+    }
+
+    class MoodListener(
+            val saidThis: (String) -> Unit,
+            val sayThis: (String) -> Unit
+
+    ) : AIListener {
+        override fun onListeningStarted() {
+            /*empty*/
+        }
+
+        override fun onAudioLevel(level: Float) {
+            /*empty*/
+        }
+
+        override fun onListeningCanceled() {
+            /*empty*/
+        }
+
+        override fun onListeningFinished() {
+            /*empty*/
+        }
+
+        var onGoodMood: () -> Unit = {}
+        var onOkMood: () -> Unit = {}
+        var onBadMood: () -> Unit = {}
+
+
+        override fun onResult(result: AIResponse) {
+            saidThis(result.result.resolvedQuery)
+            sayThis(result.result.fulfillment.speech)
+            processMood(Mood.from(result.result.action))
+        }
+
+        private fun processMood(mood: Mood) {
+            when (mood) {
+                Mood.BAD -> onBadMood()
+                Mood.OK -> onOkMood()
+                Mood.GOOD -> onGoodMood()
+            }
+        }
+
+        override fun onError(error: AIError?) {
+        }
+    }
+
+    enum class Mood(val mood: String) {
+        BAD("BadMood"), OK("OkMood"), GOOD("GoodMood");
+
+        companion object {
+
+            fun from(value: String): Mood {
+                return Mood.values()
+                        .firstOrNull { mood -> mood.mood.equals(value, true) } ?: OK
+            }
         }
     }
 
     companion object {
+        const val HI_MIKE = 0
+        const val LISTENING = 1
+        const val WHAT_WAS_SAID = 2
+
         const val DIALOG_FLOW_TOKEN = "dbb59867471149ecafd254c7263b8fd7"
+        const val INIT_SPEECH = "Hi Mike. How are you feeling today?"
     }
 }
