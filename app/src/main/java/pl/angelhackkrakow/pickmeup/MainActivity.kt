80fbd57e27bb730c5cc7e5c38ea5c71eed9e830a
Jacek Kuzniarski <jacek.kuzniarski@infullmobile.com>
2018-05-19 14:28:08 +0200

    Token key

--- app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
+++ app/src/main/java/pl/angelhackkrakow/pickmeup/MainActivity.kt
@@ -5,36 +5,40 @@ import ai.api.model.AIError
 import ai.api.model.AIResponse
 import ai.api.ui.AIButton
 import android.Manifest
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    ActivityCompat.requestPermissions(this,
-        arrayOf(Manifest.permission.RECORD_AUDIO),
-        1)
-    val config = AIConfiguration("4cc19c2db5bf4b01be4f36100dbd2f98",
-        ai.api.AIConfiguration.SupportedLanguages.English,
-        AIConfiguration.RecognitionEngine.System)
-    micButton.initialize(config)
-    micButton.setResultsListener(object : AIButton.AIButtonListener {
-      override fun onCancelled() {
-      }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                1)
+        val config = AIConfiguration(DIALOG_FLOW_TOKEN,
+                ai.api.AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System)
+        micButton.initialize(config)
+        micButton.setResultsListener(object : AIButton.AIButtonListener {
+            override fun onCancelled() {
+            }
 
-      override fun onResult(result: AIResponse?) {
-        Log.d("ApiAi", "onResult")
-      }
+            override fun onResult(result: AIResponse) {
 
-      override fun onError(error: AIError?) {
-        Log.d("ApiAi", "onError")
-      }
-    })
-  }
+                Log.d("ApiAi", "onResult")
+            }
 
+            override fun onError(error: AIError?) {
+                Log.d("ApiAi", "onError")
+            }
+        })
+    }
+
+    companion object {
+        const val DIALOG_FLOW_TOKEN = "dbb59867471149ecafd254c7263b8fd7"
+    }
 }
